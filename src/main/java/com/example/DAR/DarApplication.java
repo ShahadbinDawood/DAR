857a6231a201426ec3ad923e07462c8730485571
SHUAA <shuaa.almarwani@gmail.com>
2026-06-14 11:53:16 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/DAR/DarApplication.java
@@ -0,0 +1,13 @@
+package com.example.DAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DAR/DarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
